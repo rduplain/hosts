6fcb67c86cac695ab64e775a92713786c0fe076c
Rom DuPlain <rom.duplain@gmail.com>
2020-01-29 16:51:52 -0800

    Use latest Makefile.d.

--- .Makefile.d-init.mk
+++ .Makefile.d-init.mk
@@ -1,15 +1,14 @@
-__FILE__ := $(abspath $(lastword $(MAKEFILE_LIST)))
+MAKEFILE_D_INIT_MK := $(abspath $(lastword $(MAKEFILE_LIST)))
 
-MAKEFILE_D_URL := https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REV := v1.3.1 # Use --ref instead of --tag below if untagged.
+MAKEFILE_D_URL ?= https://github.com/rduplain/Makefile.d.git
+MAKEFILE_D_REV ?= v1.4.1 # Use --ref instead of --tag below if untagged.
 
-ifeq ($(QWERTY_SH),)
-QWERTY_SH := curl -sSL qwerty.sh | sh -s -
-endif
+QWERTY_SH_URL ?= https://qwerty.sh
+QWERTY_SH ?= curl --proto '=https' --tlsv1.2 -sSf $(QWERTY_SH_URL) | sh -s -
 
 .Makefile.d/%.mk: .Makefile.d/path.mk
 	@touch $@
 
-.Makefile.d/path.mk: $(__FILE__)
+.Makefile.d/path.mk: $(MAKEFILE_D_INIT_MK)
 	$(QWERTY_SH) -f -o .Makefile.d --tag $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
 	@touch $@
